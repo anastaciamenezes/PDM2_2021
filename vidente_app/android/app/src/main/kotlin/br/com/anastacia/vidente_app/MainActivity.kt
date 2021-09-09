1efe4040cea6f5e69ca1779875b4811cca7767ce
Anastacia Menezes <anastacia.azevedo@estudante.ifms.edu.br>
2021-09-08 23:22:59 -0300

    Versao atualizada1

---
+++ vidente_app/android/app/src/main/kotlin/br/com/anastacia/vidente_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.anastacia.vidente_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ vidente_app/lib/widgets/configuracoes.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:vidente_app/models/cidade.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
+import 'package:vidente_app/services/cidade_service.dart';
+
+
+class Configuracoes extends StatelessWidget {
+  final List<Cidade> cidades;
+  const Configuracoes({Key key, this.cidades}) : super(key: key);
+
+  //Cria uma lista de . [...]elements
+  List<Cidade> filtrarCidades(String consulta) {
+    return this.cidades.where(
+        (cidade) => cidade.nome.toLowerCase().contains(consulta.toLowerCase())).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Configurações"),
+      ),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(16, 60, 16, 0),
+        child: TypeAheadField<Cidade>(
+          textFieldConfiguration: TextFieldConfiguration(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.search),
+              border: OutlineInputBorder(),
+              hintText: 'Procurar cidade',
+            ),
+          ),
+          suggestionsCallback: filtrarCidades,
+          itemBuilder: (context, Cidade sugestao) {
+            return ListTile(
+              leading: Icon(Icons.place),
+              title: Text(sugestao.nome + " - " + sugestao.siglaEstado),
+              subtitle: Text(sugestao.estado),
+            );
+          },  
+          onSuggestionSelected: (sugestao) async {
+            CidadeService service = CidadeService();
+            final String filtro = sugestao.nome + ' ' + sugestao.estado;
+            await service.pesquisarCidade(filtro);
+            Navigator.pop(context);
+          },
+          
+          noItemsFoundBuilder: (context) => Container(
+            child: Center(
+              child: Text(
+                'Nenhuma cidade encontrada',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
